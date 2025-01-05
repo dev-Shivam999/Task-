25d7beec37734b7341d6f556f9cd3eddf46bbd06
dev-Shivam999 <shivamswami982@gmail.com>
2025-01-05 10:39:31 +0530

    today

--- client/src/components/CoverImg.tsx
+++ client/src/components/CoverImg.tsx
@@ -2,7 +2,8 @@
 const CoverImg = ({src,className,Click}:{src:string,className:string,Click:(e:string)=>void}) => {
     return (
         <img className={`${className} rounded-lg w-[50px] h-[50px]
- cursor-pointer` } onClick={()=>Click(src)} src={src} alt="" />
+
+        cursor-pointer` } onClick={()=>Click(src)} src={src} alt="" />
     );
 };
 
